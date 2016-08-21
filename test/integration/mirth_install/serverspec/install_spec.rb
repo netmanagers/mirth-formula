8c252182589b99074f023c9c30346547882cd972
Javier Bértoli <javier@netmanagers.com.ar>
2016-08-21 11:38:53 -0300

    Add tests

--- test/integration/mirth_install/serverspec/install_spec.rb
+++ test/integration/mirth_install/serverspec/install_spec.rb
@@ -6,4 +6,19 @@ describe 'mirth/install.sls' do
       it { should be_installed }
     end
   end
+
+  describe file('/opt/mirthconnect') do
+    it { should exist }
+    it { should be_directory }
+  end
+
+  describe user('mirth') do
+    it { should exist }
+    it { should belong_to_group 'mirth' }
+    it { should have_login_shell '/bin/bash' }
+  end
+
+  describe file('/home/mirth') do
+    it { should_not exist }
+  end
 end
